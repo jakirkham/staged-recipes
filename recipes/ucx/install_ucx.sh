73f3a3a453cd742de722f327372e331244e24d78
John Kirkham <jakirkham@gmail.com>
2019-04-13 00:01:03 -0400

    Add ucx recipe for CPU/GPU

---
+++ recipes/ucx/install_ucx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -z ${GIT_FULL_HASH-} ]; then
+    :;
+else
+    ./autogen.sh
+fi
+
+CUDA_CONFIG_ARG=""
+if [ ${ucx_proc_type} == "gpu" ]; then
+    CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
+fi
+
+./configure \
+    --build="${BUILD}" \
+    --host="${HOST}" \
+    --prefix="${PREFIX}" \
+    --disable-cma \
+    --disable-numa \
+    --enable-mt \
+    --with-gnu-ld \
+    ${CUDA_CONFIG_ARG}
+
+make -j${CPU_COUNT}
+make install
