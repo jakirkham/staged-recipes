6ac01025c0def797ff8dbec9d17e4e5cc2cbb8b4
Benjamin Zaitlen <quasiben@gmail.com>
2019-04-24 16:02:00 -0700

    link the rdam-core world and munge!

--- recipes/ucx/install_ucx.sh
+++ recipes/ucx/install_ucx.sh
@@ -2,16 +2,34 @@
 
 set -euo pipefail
 
-if [ -z ${GIT_FULL_HASH-} ]; then
-    :;
-else
-    ./autogen.sh
-fi
+
+#CONDA_BUILD_SYSROOT
+CONDA_BUILD_SYSROOT="$(${CC} --print-sysroot)"
+mkdir -p "${PREFIX}/lib"
+mkdir -p "${PREFIX}/include"
+#mkdir -p "${CONDA_BUILD_SYSROOT}/lib"
+#mkdir -p "${CONDA_BUILD_SYSROOT}/include"
+
+
+#ln -s /usr/lib64/librdmacm.so* ${PREFIX}/usr/lib64
+#ln -s /usr/lib64/libibverbs.so* ${PREFIX}/usr/lib64
+#ln -s /usr/include/rdma ${PREFIX}/usr/include
+#ln -s /usr/include/infiniband ${PREFIX}/usr/include
+
+ln -s /usr/lib64/librdmacm.so* ${PREFIX}/lib
+ln -s /usr/lib64/libibverbs.so* ${PREFIX}/lib
+ln -s /usr/lib64/libnl.so ${PREFIX}/lib/libnl.so.1
+ln -s /usr/include/rdma ${PREFIX}/include
+ln -s /usr/include/infiniband ${PREFIX}/include
 
 CUDA_CONFIG_ARG=""
 if [ ${ucx_proc_type} == "gpu" ]; then
     CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
 fi
+
+./autogen.sh
+
+export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,$PREFIX/lib"
 
 ./configure \
     --build="${BUILD}" \
@@ -20,10 +38,17 @@ fi
     --disable-cma \
     --disable-numa \
     --enable-mt \
+    --enable-examples \
     --with-gnu-ld \
-    --with-rdmacm \
-    --with-verbs \
+    --with-rdmacm=${PREFIX} \
+    --with-verbs=${PREFIX} \
     ${CUDA_CONFIG_ARG}
 
 make -j${CPU_COUNT}
 make install
+
+#ln -s ${PREFIX}/usr/lib64/librdmacm.so* ${PREFIX}/lib
+#ln -s /usr/include/rdma ${PREFIX}/usr/include
+#ln -s /usr/include/infiniband ${PREFIX}/usr/include
+
+
