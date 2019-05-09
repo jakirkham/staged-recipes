ca5d73e60686809e26500d987592f60fd47f7b75
John Kirkham <jakirkham@gmail.com>
2019-05-08 23:28:59 -0400

    Drop some unused flags

--- recipes/xgboost/build.sh
+++ recipes/xgboost/build.sh
@@ -20,12 +20,10 @@ cmake --verbose \
       -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON \
       -D CMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
       -D USE_CUDA:BOOL=ON \
-      -D CUDA_TOOLKIT_ROOT_DIR:PATH="${CUDA_HOME}" \
       -D CMAKE_CUDA_COMPILER:PATH="${CUDA_HOME}/bin/nvcc" \
       -D CMAKE_CUDA_HOST_COMPILER:PATH="${CXX}" \
       -D CUDA_USE_STATIC_CUDA_RUNTIME:BOOL=OFF \
       -D CUDA_PROPAGATE_HOST_FLAGS:BOOL=OFF \
-      -D CMAKE_CUDA_INCLUDE_DIRS:PATH="${PREFIX}/x86_64-conda_cos6-linux-gnu/sysroot/usr/include;${PREFIX}/include;${CUDA_HOME}/include" \
       -D CMAKE_CXX_STANDARD:STRING="11" \
       -D CMAKE_CUDA_STANDARD:STRING="11" \
       -D CMAKE_CUDA_FLAGS:STRING="" \
@@ -36,6 +34,5 @@ cmake --verbose \
       -D USE_CUDF:BOOL=ON \
       -D CUDF_ROOT:PATH="${PREFIX}" \
       -D CUDF_INCLUDE_DIR:PATH="${PREFIX}/include" \
-      -D CUDF_LIBRARY:PATH="${PREFIX}/lib/libcudf.so" \
       "${SRC_DIR}"
 make -j${CPU_COUNT}
