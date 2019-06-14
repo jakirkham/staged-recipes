ae29c1ca459fc5cfdb1ed98dc22406409686c769
John Kirkham <jakirkham@gmail.com>
2019-06-14 13:16:04 -0400

    Add UCX

---
+++ recipes/ucx/install_ucx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+export CONDA_BUILD_SYSROOT="$(${CC} --print-sysroot)"
+
+export CFLAGS="${CFLAGS} -I${CONDA_BUILD_SYSROOT}/usr/include"
+export LDFLAGS="${LDFLAGS} -L${CONDA_BUILD_SYSROOT}/usr/lib64"
+
+CUDA_CONFIG_ARG=""
+if [ ${ucx_proc_type} == "gpu" ]; then
+    CUDA_CONFIG_ARG="--with-cuda=${CUDA_HOME}"
+fi
+
+#./autogen.sh
+./configure \
+    --build="${BUILD}" \
+    --host="${HOST}" \
+    --prefix="${PREFIX}" \
+    --disable-cma \
+    --disable-numa \
+    --enable-mt \
+    --with-gnu-ld \
+    --with-rdmacm="/usr" \
+    ${CUDA_CONFIG_ARG}
+
+make -j${CPU_COUNT}
+make install
