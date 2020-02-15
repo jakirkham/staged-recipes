fd0f20c63a25659e23b967d856e5eef0412f32ab
John Kirkham <jakirkham@gmail.com>
2020-02-14 21:54:39 -0800

    Add CDT for `rdma` (needed by `libmlx4`)

---
+++ recipes/rdma-cos6-x86_64/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -o errexit -o pipefail
+
+mkdir -p "${PREFIX}"/x86_64-conda_cos6-linux-gnu/sysroot
+pushd "${PREFIX}"/x86_64-conda_cos6-linux-gnu/sysroot > /dev/null 2>&1
+cp -Rf "${SRC_DIR}"/binary/* .
